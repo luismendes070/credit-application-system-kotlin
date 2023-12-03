d3d02239ddb215d16d34c808827f9ccb87ae21aa
Luis Mendes <machadoluismendes@gmail.com>
2023-12-03 16:33:14 -0300

    Increment
    @Service

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/ennumeration/JpaRepository.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/ennumeration/JpaRepository.kt
@@ -1,11 +1,12 @@
 package me.dio.credit.application.system.ennumeration
 
 import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.entity.Customer
 import org.springframework.stereotype.Repository
 
 @Repository
 interface JpaRepository {
 
-
+    fun save(customer: Customer)
 
 }
--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
@@ -2,12 +2,16 @@ package me.dio.credit.application.system.repository
 
 import me.dio.credit.application.system.ennumeration.JpaRepository
 import me.dio.credit.application.system.entity.Credit
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
+    fun save(credit: Credit): Credit
 
+    @Query(value="SELECT * FROM CREDIT WHERE CREDIT_ID = ?l", nativeQuery = true)
+    fun findAllByCredits(customerId: Long): List<Credit>
 
 
 }
